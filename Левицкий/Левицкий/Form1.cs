9dd2e22ca3d310ae11c3254ad8ae71c20d254e9d
lsahova71-debug <lsahova71@gmail.com>
2026-03-28 17:12:27 +0300

    Update Form1.cs

--- Левицкий/Левицкий/Form1.cs
+++ Левицкий/Левицкий/Form1.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Mac.Form;
 
-namespace Левицкий
+namespace Шахова
 {
     public partial class Form1 : Form
     {
