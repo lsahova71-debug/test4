4bb3b5ae8147b1150efbd30b66d03a09a3eceadc
MrMarypoppins <vano39832@gmail.com>
2026-03-28 16:49:41 +0300

    Update Form1.cs

--- Левицкий/Левицкий/Form1.cs
+++ Левицкий/Левицкий/Form1.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Mac.Form;
 
 namespace Левицкий
 {
